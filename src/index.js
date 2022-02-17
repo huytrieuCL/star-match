adda60b4ac3ee03be9f5631a12f2a9bbd632b764
huytrieuCL <huy.trieu@codelink.io>
2022-02-17 10:21:28 +0700

    Add css for application

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./components/App";
+import "./styles/index.css";
 
 ReactDOM.hydrate(<App />, document.getElementById("mountNode"));
